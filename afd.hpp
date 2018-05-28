ec4baf6b9e2b6bed5143c0914eca96c7f19bf80a
Peter Brendel <peter_brendel@hotmail.com>
2018-05-28 17:50:23 -0300

    Update afd.hpp

--- afd.hpp
+++ afd.hpp
@@ -1,7 +1,16 @@
 #ifndef AFD_HPP
 #define AFD_HPP
 
-#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <map>
+#include <utility>
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <queue>
 #include "Utils.hpp"
 
 void game_loop();
